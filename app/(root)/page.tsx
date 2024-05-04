a7d452fbb62d29fad491ca22d002411c52e22035
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-04 10:43:08 -0300

    feat: add Banner on HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,17 @@
+import Collections from "@/components/Collections";
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <Image
+        src="/banner.png"
+        alt="banner"
+        width={2000}
+        height={1000}
+        className="w-screen"
+      />
+      <Collections />
+    </>
+  );
 }
