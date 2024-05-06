2a79bc04a4fd3e85892f754871c3ec17177f4cd3
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-06 11:58:34 -0300

    feat: add function to fetch product details by product ID

--- lib/actions.ts
+++ lib/actions.ts
@@ -10,3 +10,10 @@ export const getProducts = async () => {
   const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
   return await products.json();
 };
+
+export const getProductDetails = async (productId: string) => {
+  const product = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`
+  );
+  return await product.json();
+};
