704b5baf324917425a624487131cf1a413775d0e
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-06 10:34:36 -0300

    feat: Add user retrieval and creation functionality

---
+++ app/api/users/route.ts
@@ -0,0 +1,28 @@
+import User from "@/lib/models/User";
+import { connectToDB } from "@/lib/mongoDB";
+import { auth } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    await connectToDB();
+
+    let user = await User.findOne({ clerkId: userId });
+
+    if (!user) {
+      user = await User.create({ clerkId: userId });
+      await user.save();
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (err) {
+    console.log("[users_GET]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
