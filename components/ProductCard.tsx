39ebd130dc3bd06333f950783555bff1208c413c
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-07 10:20:09 -0300

    feat: Refactor Product Card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,85 +1,37 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
-import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import HeartFavorite from "./HeartFavorite";
 
-const ProductCard = ({ product }: { product: ProductType }) => {
-  const router = useRouter();
-  const { user } = useUser();
+interface ProductCardProps {
+  product: ProductType;
+  updateSignedInUser?: (updatedUser: UserType) => void;
+}
 
-  const [loading, setLoading] = useState(false);
-  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
-  const [isLiked, setIsLiked] = useState(false);
-
-  const getUser = async () => {
-    try {
-      if (!user) {
-        router.push("/sign-in");
-        return;
-      } else {
-        setLoading(true);
-        const res = await fetch("/api/users");
-        const data = await res.json();
-        setSignedInUser(data);
-        setIsLiked(data.wishlist.includes(product._id));
-        setLoading(false);
-      }
-    } catch (err) {
-      console.log("[users_GET", err);
-    }
-  };
-
-  useEffect(() => {
-    if (user) {
-      getUser();
-    }
-  }, [user]);
-
-  const handleLike = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    try {
-      e.preventDefault();
-
-      setLoading(true);
-      const res = await fetch("/api/users/wishlist", {
-        method: "POST",
-        body: JSON.stringify({ productId: product._id }),
-      });
-      const updatedUser = await res.json();
-      setSignedInUser(updatedUser);
-      setIsLiked(updatedUser.wishlist.includes(product._id));
-    } catch (err) {
-      console.log("[users_POST", err);
-    }
-  };
-
+const ProductCard = ({ product, updateSignedInUser }: ProductCardProps) => {
   return (
     <Link
       href={`/products/${product._id}`}
-      className="w-[220px] flex flex-col gap-2 "
+      className="w-[220px] flex flex-col gap-2"
     >
       <Image
         src={product.media[0]}
         alt="product"
         width={250}
         height={300}
-        className="h-[250px] rounded-lg object-cover "
+        className="h-[250px] rounded-lg object-cover"
       />
       <div>
         <p className="text-base-bold">{product.title}</p>
-        <p className="text-small-medium text-gray-2">{product.category}</p>
+        <p className="text-small-medium text-grey-2">{product.category}</p>
       </div>
-
       <div className="flex justify-between items-center">
         <p className="text-body-bold">${product.price}</p>
-        <button onClick={handleLike}>
-          <Heart fill={`${isLiked ? "red" : "white"} `} />
-        </button>
+        <HeartFavorite
+          product={product}
+          updateSignedInUser={updateSignedInUser}
+        />
       </div>
     </Link>
   );
