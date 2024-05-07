dbc50571d73a18d709e7cc7d39a4d0e633919b1f
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-07 11:38:49 -0300

    feat: add Toaster Provider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import NavBar from "@/components/NavBar";
+import ToasterProvider from "@/lib/providers/ToasterProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClerkProvider>
+          <ToasterProvider />
           <NavBar />
           {children}
         </ClerkProvider>
