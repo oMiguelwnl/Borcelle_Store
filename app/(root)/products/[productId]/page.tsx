a2a4952df23e0f71738fda5a56f0ff25b79214ad
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-07 10:38:42 -0300

    feat: add Product Info Component

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -1,4 +1,5 @@
 import Gallery from "@/components/Gallery";
+import ProductInfo from "@/components/ProductInfo";
 import { getProductDetails } from "@/lib/actions";
 
 const ProductDetails = async ({
@@ -11,6 +12,7 @@ const ProductDetails = async ({
   return (
     <div className="flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
       <Gallery productMedia={productDetails.media} />
+      <ProductInfo productInfo={productDetails} />
     </div>
   );
 };
