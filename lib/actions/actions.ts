230bc0f9bace86db538196677eac0ae4afa498c1
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-14 10:34:12 -0300

    feat: Add GET orders route

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -30,3 +30,10 @@ export const getSearchedProducts = async (query: string) => {
   );
   return await searchedProducts.json();
 };
+
+export const getOrders = async (customerId: string) => {
+  const orders = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`
+  );
+  return await orders.json();
+};
