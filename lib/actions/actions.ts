d9d0c8d2ae118df2d657166fefccaf4922a16b2e
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-05-14 11:03:09 -0300

    feat: Add GET Related Products route

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -37,3 +37,10 @@ export const getOrders = async (customerId: string) => {
   );
   return await orders.json();
 };
+
+export const getRelatedProducts = async (productId: string) => {
+  const relatedProducts = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`
+  );
+  return await relatedProducts.json();
+};
